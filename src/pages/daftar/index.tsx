ae7495bd25a420635b7bc5e4636f576756a535d6
Devaaji <devaaji5@gmail.com>
2023-04-16 10:11:09 +0700

    [ADD] ssr condision isReady Account

--- src/pages/daftar/index.tsx
+++ src/pages/daftar/index.tsx
@@ -1,3 +1,24 @@
 import Daftar from '@/component/pages/Daftar';
 
+export async function getServerSideProps() {
+    // Fetch content data based on slug parameter
+    const isReadyAccount = false;
+
+    if (!isReadyAccount) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        redirect: {
+            destination: "/",
+            permanent: false,
+        },
+    };
+}
+
 export default Daftar
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,25 @@
 import Login from '@/component/pages/Login';
 
+
+export async function getServerSideProps() {
+    // Fetch content data based on slug parameter
+    const isReadyAccount = false;
+
+    if (!isReadyAccount) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        redirect: {
+            destination: "/",
+            permanent: false,
+        },
+    };
+}
+
 export default Login
