e27a0150ad7d688569c905dd4fb6e70ed376d219
Devaaji <devaaji5@gmail.com>
2023-04-19 16:07:13 +0700

    [ADD] Logo Spill Light

---
+++ src/component/elements/SpillLogo/index.tsx
@@ -0,0 +1,30 @@
+import NextImage from '../NextImage';
+import SpillLogoLight from '../../../assets/images/Logo.png';
+
+
+type SpillLogoProps = {
+    multiplySize?: number;
+    responsive?: boolean;
+};
+
+const SpillLogo = ({
+    multiplySize = 1,
+    responsive = false,
+}: SpillLogoProps) => {
+
+    const baseWidth = 228 * multiplySize;
+    const baseHeight = 55 * multiplySize;
+
+    return (
+        <NextImage
+            src={SpillLogoLight}
+            alt="Vocasia"
+            width={baseWidth}
+            height={baseHeight}
+            layout={responsive ? 'responsive' : undefined}
+            priority
+        />
+    )
+}
+
+export default SpillLogo
