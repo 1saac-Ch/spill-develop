2cc5a941add22dd280f20aba18092893c380ff42
Devaaji <devaaji5@gmail.com>
2023-04-21 08:47:54 +0700

    [ADD]  Homepage

--- src/component/pages/Home/index.tsx
+++ src/component/pages/Home/index.tsx
@@ -1,12 +1,21 @@
-import styles from './index.module.scss'
-import Layout from '@/component/layouts'
-export default function Counter() {
+import { ReactElement, JSXElementConstructor } from 'react';
+import MainLayout from '@/component/layouts/MainLayout';
+import styles from "./index.module.scss";
+import Search from "@/component/elements/Search";
+
+const Home = () => {
   return (
-    <div>
-      <h1 className={styles.header}>
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <div className={styles.wording}>
+        <h1>Cari produk, Baca review, Checkout, lalu <label>Spill</label> disini.</h1>
+        <p>Spill adalah tempat buat bantu kamu yang bingung mau checkout produk apa</p>
+        <Search variant="wording" placeholder="Cari produk apapun" />
+      </div>
+    </>
   )
 }
 
+Home.getLayout = (page: ReactElement<any, string | JSXElementConstructor<any>>) =>
+  <MainLayout>{page}</MainLayout>;
+
+export default Home;
--- src/component/pages/LandingPage/index.tsx
+++ src/component/pages/LandingPage/index.tsx
@@ -1,17 +1,17 @@
-import Layouts from '../../layouts';
+
 import styles from "./styles.module.scss";
 import Search from "@/component/elements/Search";
 function LandingPage() {
   return (
-    <Layouts>
-      <div className={styles.root}>
+    <div className={styles.root}>
+      <div className={styles.container}>
         <div className={styles.wording}>
           <h1>Cari produk, Baca review, Checkout, lalu <label>Spill</label> disini.</h1>
           <p>Spill adalah tempat buat bantu kamu yang bingung mau checkout produk apa</p>
-          <Search variant="wording" placeholder="Cari produk apapun"/>
+          <Search variant="wording" placeholder="Cari produk apapun" />
         </div>
       </div>
-    </Layouts>
+    </div>
   )
 }
 
