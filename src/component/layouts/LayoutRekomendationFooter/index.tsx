604b2daaa7310d3b02d923d85657fe47a2c08058
Devaaji <devaaji5@gmail.com>
2023-05-20 19:50:09 +0700

    [FIX] Styles Recomend Product

--- src/component/layouts/LayoutRekomendationFooter/index.tsx
+++ src/component/layouts/LayoutRekomendationFooter/index.tsx
@@ -1,21 +1,22 @@
-import Button from '@/component/elements/Button'
 import React from 'react'
+import styles from "./styles.module.scss"
+import Button from '@/component/elements/Button'
 
 const LayoutRekomendationFooter = () => {
   return (
-    <div className="flex justify-center bg-[#E8FBF5]">
-        <div className='container mx-auto'>
-          <div className='flex justify-between items-center my-8'>
-            <div className='flex-[1.8]'>
-              <h1 className='text-xl font-tebal'> 🔍 Produk yang kamu cari tidak ada di spill?</h1>
-              <p> ayo bantu sarankan kami untuk menuliskan produk yang kamu cari</p>
-            </div>
-            <div className='flex-[0.2]'>
-              <Button>Sarankan Produk</Button>
-            </div>
+    <div className={styles.layoutRecomend}>
+      <div className={styles.layoutContainer}>
+        <div className={styles.mainRecomend}>
+          <div className={styles.stackResponsive}>
+            <h1 className={styles.textFindProduct}> 🔍 Produk yang kamu cari tidak ada di spill?</h1>
+            <p> ayo bantu sarankan kami untuk menuliskan produk yang kamu cari</p>
+          </div>
+          <div className={styles.buttonResponsi}>
+            <Button className={styles.buttonRecomended}>Sarankan Produk</Button>
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
