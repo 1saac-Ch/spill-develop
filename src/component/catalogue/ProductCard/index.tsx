ee3d5a2ad139498b6eafd9c2ca69ccf24b8b8852
aguswesly <agustchannel@gmail.com>
2023-05-30 09:37:03 +0700

    adding not-found page

--- src/component/catalogue/ProductCard/index.tsx
+++ src/component/catalogue/ProductCard/index.tsx
@@ -1,22 +1,38 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import ComputerImage from '@/assets/images/computer.jpg'
 import StarFilled from '@/assets/icons/StarFilled.svg'
 
-const ProductCard = () => {
+type Props = {
+  image: string | StaticImageData
+  title: string
+  production: string
+  rate: number
+  review: string
+  minPrize: string
+  maxPrize: string
+}
+
+const ProductCard = ({
+  image,
+  title,
+  production,
+  rate,
+  review,
+  maxPrize,
+  minPrize,
+}: Props) => {
   return (
     <div className="max-w-[240px] bg-white rounded-xl shadow-medium cursor-pointer">
       <Image
-        src={ComputerImage}
+        src={image}
         alt="product-computer"
         className="w-full aspect-square object-cover"
       />
 
       <div className="p-4 flex flex-col gap-2 w-full">
-        <h3 className="text-label-md font-bold max-w-44">
-          Sony VM-1000x Wireless Headphone Bluetooth
-        </h3>
+        <h3 className="text-label-md font-bold max-w-44">{title}</h3>
 
-        <p className="text-label-md font-satoshi">Sony</p>
+        <p className="text-label-md font-satoshi">{production}</p>
 
         <div className="flex gap-2 items-center">
           <div className="flex gap-2 items-center">
@@ -25,16 +41,16 @@ const ProductCard = () => {
               className="w-3 h-3 object-contain"
               alt="star"
             />
-            <p className="text-body-sm font-satoshi">4.8</p>
+            <p className="text-body-sm font-satoshi">{rate}</p>
           </div>
 
           <span>|</span>
 
-          <p className="text-body-sm font-satoshi">32 Reviews</p>
+          <p className="text-body-sm font-satoshi">{review}</p>
         </div>
 
         <h4 className="text-label-md text-pink font-bold">
-          Rp.1000.000 - Rp.1.430.000
+          {minPrize} - {maxPrize}
         </h4>
       </div>
     </div>
--- src/component/pages/CatalogueProduct/index.tsx
+++ src/component/pages/CatalogueProduct/index.tsx
@@ -6,6 +6,8 @@ import CatalogueLayout from '@/component/layouts/LayoutCatalogue'
 
 import LeftArrow from '@/assets/icons/LeftArrow.svg'
 import RightArrow from '@/assets/icons/RightArrow.svg'
+import ComputerImage from '@/assets/images/computer.jpg'
+
 import Image from 'next/image'
 import {
   Select,
@@ -18,6 +20,7 @@ import Link from 'next/link'
 import FilterProduct from '@/component/catalogue/FilterProduct'
 import ProductCard from '@/component/catalogue/ProductCard'
 import RecommendProduct from '@/component/catalogue/RecommendProduct'
+import NotFoundProduct from './not-found'
 
 const SortOptions = [
   'Paling Sesuai',
@@ -30,86 +33,101 @@ const SortOptions = [
 const CatalogueProduct = () => {
   const [activeOption, setActiveOption] = React.useState(SortOptions[0])
 
+  const notFound = false
+
   return (
     <main className="bg-background">
       <div className="min-h-screen grid grid-cols-[253px_1fr] gap-10 px-5 md:pl-12 md:pr-[72px] py-16 font-satoshi">
         <FilterProduct />
 
-        <div className="flex flex-col gap-6">
-          <div className="flex items-center justify-between ">
-            <h4 className="text-title-md font-bold">
-              Menampilkan <span>count</span> untuk kata kunci{' '}
-              <span>keyword</span>
-            </h4>
+        {!notFound ? (
+          <div className="flex flex-col gap-6">
+            <div className="flex items-center justify-between ">
+              <h4 className="text-title-md font-bold">
+                Menampilkan <span>count</span> untuk kata kunci{' '}
+                <span>keyword</span>
+              </h4>
 
-            <div className="flex items-center gap-6">
-              <label className="text-title-md font-[900]">Urutkan</label>
-              <Select
-                onValueChange={(val) => setActiveOption(val)}
-                value={activeOption}
-              >
-                <SelectTrigger className="w-[200px]">
-                  <SelectValue placeholder="Paling sesuai" />
-                </SelectTrigger>
-                <SelectContent>
-                  {SortOptions.map((opt, i) => (
-                    <SelectItem
-                      key={i}
-                      value={opt}
-                      className={opt === activeOption ? 'font-bold' : ''}
-                      defaultChecked={i === 0}
-                    >
-                      {opt}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+              <div className="flex items-center gap-6">
+                <label className="text-title-md font-[900]">Urutkan</label>
+                <Select
+                  onValueChange={(val) => setActiveOption(val)}
+                  value={activeOption}
+                >
+                  <SelectTrigger className="w-[200px]">
+                    <SelectValue placeholder="Paling sesuai" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {SortOptions.map((opt, i) => (
+                      <SelectItem
+                        key={i}
+                        value={opt}
+                        className={opt === activeOption ? 'font-bold' : ''}
+                        defaultChecked={i === 0}
+                      >
+                        {opt}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            {/* CARDS */}
+            <div className="grid grid-cols-4 gap-4 mb-4">
+              {Array(5)
+                .fill(0)
+                .map((_, i) => (
+                  <ProductCard
+                    key={i}
+                    image={ComputerImage}
+                    title="Sony VM-1000x Wireless Headphone Bluetooth"
+                    review="32"
+                    rate={4.8}
+                    production="Sony"
+                    minPrize="Rp.1.000.000"
+                    maxPrize="Rp.1.430.000"
+                  />
+                ))}
+            </div>
+
+            {/* PAGINATION */}
+            <div className="flex items-center gap-4 ml-auto mb-14">
+              <Link href={'/'}>
+                <Image
+                  src={LeftArrow}
+                  className="w-4 h-4 object-contain"
+                  alt="left-arrow"
+                />
+              </Link>
+
+              {Array(5)
+                .fill(0)
+                .map((_, i) => (
+                  <Link
+                    key={i}
+                    href={'/'}
+                    className={`${
+                      i === 0
+                        ? 'bg-blue-50 text-white'
+                        : 'bg-transparent text-dark border border-abu'
+                    } w-11 h-11  flex items-center justify-center rounded-xl text-label-lg font-bold`}
+                  >
+                    {i + 1}
+                  </Link>
+                ))}
+              <Link href={'/'}>
+                <Image
+                  src={RightArrow}
+                  className="w-4 h-4 object-contain"
+                  alt="right-arrow"
+                />
+              </Link>
             </div>
           </div>
-
-          {/* CARDS */}
-          <div className="grid grid-cols-4 gap-4 mb-4">
-            {Array(5)
-              .fill(0)
-              .map((_, i) => (
-                <ProductCard key={i} />
-              ))}
-          </div>
-
-          {/* PAGINATION */}
-          <div className="flex items-center gap-4 ml-auto mb-14">
-            <Link href={'/'}>
-              <Image
-                src={LeftArrow}
-                className="w-4 h-4 object-contain"
-                alt="left-arrow"
-              />
-            </Link>
-
-            {Array(5)
-              .fill(0)
-              .map((_, i) => (
-                <Link
-                  key={i}
-                  href={'/'}
-                  className={`${
-                    i === 0
-                      ? 'bg-blue-50 text-white'
-                      : 'bg-transparent text-dark border border-abu'
-                  } w-11 h-11  flex items-center justify-center rounded-xl text-label-lg font-bold`}
-                >
-                  {i + 1}
-                </Link>
-              ))}
-            <Link href={'/'}>
-              <Image
-                src={RightArrow}
-                className="w-4 h-4 object-contain"
-                alt="right-arrow"
-              />
-            </Link>
-          </div>
-        </div>
+        ) : (
+          <NotFoundProduct />
+        )}
       </div>
 
       <RecommendProduct />
