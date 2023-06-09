9fcc4c1a2c64e6c2ccb4caac335830b46a27137d
aguswesly <agustchannel@gmail.com>
2023-06-09 22:40:30 +0700

    adding content

--- src/component/elements/Card/component.tsx
+++ src/component/elements/Card/component.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import NextImage from '../NextImage';
-import product from "@/assets/images/product.png"
+import NextImage from '../NextImage'
+import product from '@/assets/images/product.png'
 type CardProps = {
+  imgUrl: string
   children: React.ReactNode
 }
 
 const Card = (props: CardProps) => {
   return (
-    <div className='h-full cursor-pointer border-2 hover:border-blue-50 rounded-xl'>
-      <div className='h-full bg-white overflow-hidden rounded-2xl shadow-[0px_4px_16px_rgba(77,77,77,0.12)]'>
+    <div className="h-full cursor-pointer border-2 hover:border-blue-50 rounded-xl">
+      <div className="h-full bg-white overflow-hidden rounded-2xl shadow-[0px_4px_16px_rgba(77,77,77,0.12)]">
         <div className="flex flex-col h-full">
           <div>
-            <NextImage
-              src={product}
+            {/* eslint-disable @next/next/no-img-element */}
+            <img
+              src={props.imgUrl}
               alt="Man looking at item at a store"
               width={400}
               height={400}
-              layout="responsive"
-              className="object-cover"
+              // layout="responsive"
+              className="object-cover w-[400px] h-[400px]"
             />
           </div>
           {props.children}
@@ -25,7 +27,6 @@ const Card = (props: CardProps) => {
       </div>
     </div>
   )
-
 }
 
 export default Card
--- src/component/pages/Home/dummy.api.ts
+++ src/component/pages/Home/dummy.api.ts
@@ -1,34 +1,40 @@
-import Artikel from "@/assets/images/artikel.png";
-import Youtube from "@/assets/images/youtube.png";
+import Artikel from '@/assets/images/artikel.png'
+import Youtube from '@/assets/images/youtube.png'
 
-import Box1 from "@/assets/images/1.png";
-import Box2 from "@/assets/images/2.png";
-import Box3 from "@/assets/images/3.png";
-import Box4 from "@/assets/images/4.png";
-import Box5 from "@/assets/images/5.png";
-import Box6 from "@/assets/images/6.png";
+import Box1 from '@/assets/images/1.png'
+import Box2 from '@/assets/images/2.png'
+import Box3 from '@/assets/images/3.png'
+import Box4 from '@/assets/images/4.png'
+import Box5 from '@/assets/images/5.png'
+import Box6 from '@/assets/images/6.png'
 
 export const hotriview = [
   {
     slide: 0,
     card: [
       {
-        title: "Review Produk",
+        title: 'Apple Iphone 13 Mini',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'iPhone 13. Sistem kamera ganda paling canggih yang pernah ada di iPhone. Chip A15 Bionic yang secepat kilat. Lompatan besar dalam kekuatan baterai. Desain kokoh. Dan layar Super Retina XDR yang lebih cerah.',
         rating: 4,
+        imgUrl:
+          'https://cdn.eraspace.com/media/catalog/product/i/p/iphone_13_mini_pink_1.jpg',
       },
       {
-        title: "Sony WM-1000X",
+        title: 'Samsung Galaxy Z Fold 3',
         description:
-          "Varius at ut . Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'Smartphone lipat dengan layar Dynamic AMOLED 2X yang luas dan fleksibel. Dilengkapi dengan S Pen. Kinerja yang sangat cepat dengan chip Snapdragon 888. Pengalaman multitasking yang lebih baik.',
         rating: 5,
+        imgUrl:
+          'https://images.samsung.com/pk/smartphones/galaxy-z-fold3-5g/buy/zfold3_carousel_mainsinglekv_mo.jpg',
       },
       {
-        title: "Review Produk",
+        title: 'Sony WH-1000XM4',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'Wireless noise-canceling headphones dengan teknologi canggih untuk kualitas suara yang superior. Fitur ANC adaptif dan pemrosesan audio berbasis AI untuk pengalaman mendengarkan yang luar biasa.',
         rating: 3,
+        imgUrl:
+          'https://images.firstpost.com/fpimages/1200x800/fixed/jpg/2020/09/Sony-WH-1000XM4-type-c-min.jpg',
       },
     ],
   },
@@ -36,22 +42,27 @@ export const hotriview = [
     slide: 1,
     card: [
       {
-        title: "Review Produk",
+        title: 'Nintendo Switch OLED',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'Konsol game portabel dengan layar OLED yang lebih besar dan jelas. Dukungan docking untuk bermain di TV. Kompatibilitas dengan semua game Nintendo Switch.',
         rating: 4,
+        imgUrl:
+          'https://assets.nintendo.com/image/upload/f_auto/q_auto/dpr_2.0/c_scale,w_500/ncom/en_US/switch/videos/heg001-07060600/posters/oled-model',
       },
       {
-        title: "Review Produk",
+        title: 'DJI Mini 2',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'Drone kompak dengan kemampuan perekaman video 4K. Dilengkapi dengan sistem pemindai penghambat dan teknologi kecerdasan buatan untuk penerbangan yang aman dan stabil.',
         rating: 5,
+        imgUrl: 'https://cdn.mos.cms.futurecdn.net/qPDUPGiA6qqdzFEMWryRUb.jpg',
       },
       {
-        title: "Review Produk",
+        title: 'Samsung Galaxy Watch 4',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'Smartwatch dengan desain yang elegan dan fitur pemantauan kesehatan yang canggih. Dilengkapi dengan sensor detak jantung, pemantauan tidur, GPS, dan tahan air.',
         rating: 4,
+        imgUrl:
+          'https://images.samsung.com/id/galaxy-watch4/feature/galaxy-watch4-green-design-your-own-mo.jpg',
       },
     ],
   },
@@ -59,26 +70,32 @@ export const hotriview = [
     slide: 2,
     card: [
       {
-        title: "Review Produk",
+        title: 'Sony PlayStation 5',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'Konsol game generasi terbaru dengan performa tinggi. Pengalaman gaming yang lebih realistis dengan grafis 4K dan ray tracing. Dukungan untuk game terbaru dan virtual reality.',
         rating: 4,
+        imgUrl:
+          'https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/jawapos/2020/09/ps5.jpeg',
       },
       {
-        title: "Review Produk",
+        title: 'GoPro Hero 9 Black',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+          'Kamera aksi dengan kualitas video 5K dan fitur stabilisasi gambar HyperSmooth 3.0. Dilengkapi dengan layar sentuh, kontrol suara, dan tahan air.',
+        rating: 3,
+        imgUrl:
+          'https://1.bp.blogspot.com/-i6dPFCzem-E/X2TKoYvZpSI/AAAAAAAAB0w/X6wSHTZAgGsGZ8uvbyVbU1TMc4vrNozzACLcBGAsYHQ/s1800/GoPro%2B9%2BHero.jpg',
+      },
+      {
+        title: 'LG C1 OLED TV',
+        description:
+          'TV OLED dengan kualitas gambar yang luar biasa dan kontras yang tinggi. Dilengkapi dengan Dolby Vision, HDR10, dan fitur AI ThinQ untuk pengalaman menonton yang immersive.',
         rating: 5,
-      },
-      {
-        title: "Review Produk",
-        description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
-        rating: 4,
+        imgUrl:
+          'https://images.samsung.com/pk/smartphones/galaxy-z-fold3-5g/buy/zfold3_carousel_mainsinglekv_mo.jpg',
       },
     ],
   },
-];
+]
 
 export const feature = [
   { image: Box1 },
@@ -87,64 +104,71 @@ export const feature = [
   { image: Box4 },
   { image: Box5 },
   { image: Box6 },
-];
+]
 
 export const artikel = [
   {
     col: 1,
     image: Artikel,
-    title: "10 Rekomendasi Headphone",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus nisl, vestibulum nec finibus ac, condimentum a erat. Donec non tellus elementum, tincidunt libero ut, pulvinar dolor. Proin ultricies sem quis vehicula lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus nisl, vestibulum nec finibus ac, condimentum a erat. Donec non tellus elementum, tincidunt libero ut, pulvinar dolor. Proin ultricies sem quis vehicula lacinia.`,
+    title: '10 Rekomendasi HP Harga 4 Jutaan Terbaik (Juni 2023)',
+    description: `Rekomendasi 10 HP terbaik dengan harga 4 jutaan rupiah pada bulan Juni 2023. HP yang direkomendasikan memiliki spesifikasi yang mumpuni, seperti chipset yang bertenaga, RAM besar, dan sudah NFC. Beberapa HP bahkan sudah memiliki layar AMOLED yang menawarkan tampilan memukau dan kamera dengan resolusi 108 MP yang didukung teknologi OIS. Beberapa HP yang direkomendasikan di antaranya iQOO Z7 5G, Redmi Note 12 Pro 5G, Samsung A34, Vivo V23e, Realme 10 Pro, Poco F4, Samsung Galaxy A33 5G, Oppo Reno8 T 4G, dan Xiaomi 11T.`,
+    imgUrl:
+      'https://cdn.idntimes.com/content-images/community/2021/07/samsung-galaxy-a52-2567c83b34c54e7f328e8bdc39e08683-9f917b8fc022d1c9fa39d90ea4f7e2f7_600x400.jpg',
   },
   {
     col: 2,
     card: [
       {
-        title: "Review Produk",
+        title: '10 Laptop Gaming Terbaik di Dunia pada Tahun 2023',
         description:
-          "Lorem ipsum dolit  Semper sit scelerisque ornare nibh nisi ",
-        image: Artikel,
+          'Ini dia yang kamu cari-cari selama ini rekomendasi laptop gaming terbaik dengan spesifikasi yang mumpuni pada tahun 2023',
+        imgUrl:
+          'https://s2.bukalapak.com/uploads/content_attachment/2cad1e9b10e8d7624a7ae3c5/original/Legion_Y730.jpg',
       },
       {
-        title: "Review Produk",
+        title: '10 Rekomendasi Headphone Kualitas Suara Mantab Terbaik 2023',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
-        image: Artikel,
+          'Rekomendasi 10 headphone dengan kualitas suara terbaik dari berbagai merek seperti Sony, Apple, dan Bose pada tahun 2023.',
+        imgUrl:
+          'https://cdn1.productnation.co/stg/sites/5/JBL-Everest-300-BT.jpg',
       },
       {
-        title: "Review Produk",
+        title: 'Aneka Gebrakan Teknologi Baru Apple di WWDC 2023',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi dolor sit amet consectetur. Varius at ut vitae tellus dolor sit amet consectetur. Varius at ut vitae tellus dolor sit amet consectetur. Varius at ut vitae tellus",
-        image: Artikel,
+          'Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi dolor sit amet consectetur. Varius at ut vitae tellus dolor sit amet consectetur. Varius at ut vitae tellus dolor sit amet consectetur. Varius at ut vitae tellus',
+        imgUrl:
+          'https://awsimages.detik.net.id/community/media/visual/2023/06/06/apple-vision-pro-5.jpeg?w=1200',
       },
       {
-        title: "Review Produk",
+        title: '12 Kamera Mirrorless Terbaik Untuk Pemula 2023',
         description:
-          "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi dolor sit amet consectetur. Varius at ut vitae tellus dolor sit amet consectetur. Varius at ut vitae tellus dolor sit amet consectetur. Varius at ut vitae tellus",
-        image: Artikel,
+          'Rekomendasi 12 kamera mirrorless terbaik untuk pemula pada tahun 2023, seperti Fujifilm X-T30 II, Canon EOS R10, dan Sony ZV-E10.',
+        imgUrl:
+          'https://ceklist.id/wp-content/uploads/2021/09/Kamera-Mirrorless-Terbaik.jpg',
       },
     ],
   },
-];
+]
 export const youtube = [
   {
-    image: Youtube,
-    title: "Review Produk",
+    imgUrl:
+      'https://s.yimg.com/uu/api/res/1.2/8s_G_Iy3D63E9kTYtFFwNQ--~B/aD0xMTkyO3c9MTgwMDthcHBpZD15dGFjaHlvbg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2021-02/43a757f0-6494-11eb-ae9a-9a88d8a5050c.cf.jpg',
+    title: 'DJI Air 2S',
     description:
-      "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+      'Kamera mirrorless dengan sensor full-frame 45.7MP dan sistem fokus yang cepat dan akurat. Dilengkapi dengan layar sentuh yang dapat ditekuk dan stabilisasi gambar 5-axis. ',
   },
   {
-    image: Youtube,
-    title: "Review Produk",
+    imgUrl:
+      'https://m.media-amazon.com/images/I/51UuqFC7TuL._AC_UF894,1000_QL80_.jpg',
+    title: 'Xiaomi Mi Electric Scooter Pro 2',
     description:
-      "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+      'Scooter listrik dengan desain yang elegan dan performa yang tangguh. Dilengkapi dengan motor 300W dan baterai tahan lama untuk perjalanan yang nyaman dan efisien.',
   },
   {
-    image: Youtube,
-    title: "Review Produk",
+    imgUrl:
+      'https://images.samsung.com/pk/smartphones/galaxy-s21/buy/s21_ultra_basicgroup_kv_mo_img.jpg',
+    title: 'Samsung Galaxy S21 Ultra',
     description:
-      "Lorem ipsum dolor sit amet consectetur. Varius at ut vitae tellus fermentum. Semper sit scelerisque ornare nibh nisi. Semper sit scelerisque ornare nibh nisi ",
+      'Smartphone flagship dengan performa tinggi dan kamera yang canggih. Dilengkapi dengan layar Dynamic AMOLED 2X yang besar, prosesor Exynos 2100/Qualcomm Snapdragon 888, dan dukungan S Pen.',
   },
-];
+]
