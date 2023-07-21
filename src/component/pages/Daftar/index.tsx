a6915c2d187fc23e35e6ff265d0e97ba9c4edfe0
aguswesly <agustchannel@gmail.com>
2023-07-21 20:28:21 +0700

    adding verfication input ui

--- src/component/pages/Daftar/index.tsx
+++ src/component/pages/Daftar/index.tsx
@@ -1,45 +1,141 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import NextLink from 'next/link'
-import Link from "next/link";
+import Link from 'next/link'
 
-import styles from "./index.module.scss";
+import styles from './index.module.scss'
 
-import SpillLogo from "@/component/elements/SpillLogo";
-import TextInput from "@/component/elements/TextInput";
-import Button from "@/component/elements/Button";
-import { regex } from "@/utils/regex";
+import SpillLogo from '@/component/elements/SpillLogo'
+import TextInput from '@/component/elements/TextInput'
+import Button from '@/component/elements/Button'
+import { regex } from '@/utils/regex'
+import { cn } from '@/utils/classname'
 
-function Component() {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [noHp, setNoHp] = useState<string>("");
-  const [confirmPassword, setConfirmPassword] = useState<string>("");
-
+function Daftar() {
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [name, setName] = useState<string>('')
+  const [noHp, setNoHp] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
   const _handleChange = (e: any) => {
-
     let id = e.target.id
     let value = e.target.value
-    if (id === "username") {
-      setUsername(value);
-    } else if (id === "password") {
+    if (id === 'username') {
+      setUsername(value)
+    } else if (id === 'password') {
       if (regex.password.test(value)) {
-        setPassword(value);
+        setPassword(value)
       }
-    } else if (id === "name") {
-      setName(value);
-    } else if (id === "noHp") {
+    } else if (id === 'name') {
+      setName(value)
+    } else if (id === 'noHp') {
       if (regex.number.test(value)) {
-        setNoHp(value);
+        setNoHp(value)
       }
-      if (noHp.length === 1 && e.nativeEvent.inputType === "deleteContentBackward") {
-        setNoHp("");
+      if (
+        noHp.length === 1 &&
+        e.nativeEvent.inputType === 'deleteContentBackward'
+      ) {
+        setNoHp('')
       }
-    } else if (id === "confirmPassword") {
-      setConfirmPassword(value);
+    } else if (id === 'confirmPassword') {
+      setConfirmPassword(value)
     }
   }
+  return (
+    <>
+      <h1>Daftar di Spill 🚀</h1>
+      <p>Bisa cari produk apapun, tanpa ragu checkoutnya!</p>
+      <TextInput
+        label="Nama"
+        placeholder="Tuliskan nama kamu"
+        variant="normal"
+        value={name}
+        id="name"
+        onChange={_handleChange}
+      />
+      <TextInput
+        label="Username"
+        placeholder="Tuliskan username"
+        variant="normal"
+        value={username}
+        id="username"
+        onChange={_handleChange}
+      />
+      <TextInput
+        label="Nomor Handphone (WhatsApp)"
+        placeholder="08129389128"
+        variant="normal"
+        value={noHp}
+        id="noHp"
+        onChange={_handleChange}
+      />
+      <TextInput
+        label="Password"
+        variant="password"
+        placeholder="Tuliskan password kamu"
+        value={password}
+        id="password"
+        onChange={(e: any) => setPassword(e.target.value)}
+      />
+      <TextInput
+        label="Konfirmasi Password"
+        placeholder="Your Confirmation Password Here"
+        variant="password"
+        value={confirmPassword}
+        id="confirmPassword"
+        onChange={(e: any) => setConfirmPassword(e.target.value)}
+      />
+      <div className={styles.checkbox}>
+        <p>
+          Dengan mendaftar, saya menyetujui
+          <Link href="/syarat-kententuan">Syarat dan Ketentuan</Link>&
+          <Link href="/kebijakan-privasi">Kebijakan Privasi</Link>
+        </p>
+      </div>
+      <div className={styles.bottom}>
+        <Button>Login</Button>
+        <p>
+          Sudah punya akun Spill?<Link href="/login">Login</Link>
+        </p>
+      </div>
+    </>
+  )
+}
 
+function Verification() {
+  const [otp, setOtp] = useState(new Array(4).fill(''))
+
+  return (
+    <>
+      <div className="space-y-2">
+        <h1 className="text-headline-md font-bold">Kode Verifikasi</h1>
+        <p className="text-title-md font-satoshi">
+          Kode dikirimkan melalui WhatsApp, mohon lakukan pengecekan
+        </p>
+      </div>
+
+      <div className="flex gap-5 justify-center">
+        {otp.map((val, idx) => (
+          <input
+            key={idx}
+            type="text"
+            className={
+              'appearance-none w-[60px] h-11 rounded-xl border border-neutral-600 text-center'
+            }
+          />
+        ))}
+      </div>
+
+      <Button>Verfikasi</Button>
+      <p className="text-label-lg text-muted-foreground">
+        Mohon tunggu 29 detik untuk kirim ulang
+      </p>
+    </>
+  )
+}
+
+function Component() {
+  const [registerActive, setRegisterActive] = useState(false)
   return (
     <div className={styles.root}>
       <div className={styles.header}>
@@ -47,24 +143,17 @@ function Component() {
           <SpillLogo multiplySize={0.5} isDark={true} />
         </NextLink>
       </div>
-      <div className={styles.wrapper}>
-        <h1>Daftar di Spill 🚀</h1>
-        <p>Bisa cari produk apapun, tanpa ragu checkoutnya!</p>
-        <TextInput label="Nama" placeholder="Tuliskan nama kamu" variant="normal" value={name} id="name" onChange={_handleChange} />
-        <TextInput label="Username" placeholder="Tuliskan username" variant="normal" value={username} id="username" onChange={_handleChange} />
-        <TextInput label="Nomor Handphone (WhatsApp)" placeholder="08129389128" variant="normal" value={noHp} id="noHp" onChange={_handleChange} />
-        <TextInput label="Password" variant="password" placeholder="Tuliskan password kamu" value={password} id="password" onChange={(e: any) => setPassword(e.target.value)} />
-        <TextInput label="Konfirmasi Password" placeholder="Your Confirmation Password Here" variant="password" value={confirmPassword} id="confirmPassword" onChange={(e: any) => setConfirmPassword(e.target.value)} />
-        <div className={styles.checkbox}>
-          <p>Dengan mendaftar, saya menyetujui<Link href="/syarat-kententuan">Syarat dan Ketentuan</Link>&<Link href="/kebijakan-privasi">Kebijakan Privasi</Link></p>
+      {registerActive ? (
+        <div className={styles.wrapper}>
+          <Daftar />
         </div>
-        <div className={styles.bottom}>
-          <Button>Login</Button>
-          <p>Sudah punya akun Spill?<Link href="/login">Login</Link></p>
+      ) : (
+        <div className="absolute top-[35%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[600px] rounded-[20px] p-[40px] text-center space-y-5 font-satoshi">
+          <Verification />
         </div>
-      </div>
+      )}
     </div>
   )
 }
 
-export default Component;
+export default Component
